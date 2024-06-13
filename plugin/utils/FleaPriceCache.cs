3933196c725517248352b3fc31cc79e113348d60
RP <rodrigo-peralta@hotmail.com.ar>
2024-06-13 20:26:06 +0200

    Major refactor of plugin base Added support for repair kits and fuel durability Added support to sell items to trader if certain conditions are met

---
+++ plugin/utils/FleaPriceCache.cs
@@ -0,0 +1,79 @@
+﻿using Aki.Common.Http;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aki.Reflection.Utils;
+
+namespace LootValue
+{
+	internal static class FleaPriceCache
+	{
+		static Dictionary<string, CachePrice> cache = new Dictionary<string, CachePrice>();
+		public static ISession Session => ClientAppUtils.GetMainApp().GetClientBackEndSession();
+
+		public static int? FetchPrice(string templateId)
+		{
+			bool fleaAvailable = Session.RagFair.Available || LootValueMod.ShowFleaPriceBeforeAccess.Value;
+
+			if (!fleaAvailable)
+				return null;
+
+			if (cache.ContainsKey(templateId))
+			{
+				double secondsSinceLastUpdate = (DateTime.Now - cache[templateId].lastUpdate).TotalSeconds;
+				if (secondsSinceLastUpdate > 300)
+					return QueryAndTryUpsertPrice(templateId, true);
+				else
+					return cache[templateId].price;
+			}
+			else
+				return QueryAndTryUpsertPrice(templateId, false);
+		}
+
+		private static string QueryPrice(string templateId)
+		{
+			return RequestHandler.PostJson("/LootValue/GetItemLowestFleaPrice", JsonConvert.SerializeObject(new FleaPriceRequest(templateId)));
+		}
+
+		private static int? QueryAndTryUpsertPrice(string templateId, bool update)
+		{
+			string response = QueryPrice(templateId);
+			bool hasPlayerFleaPrice = !(string.IsNullOrEmpty(response) || response == "null");
+
+			int price;
+			if (hasPlayerFleaPrice)
+			{
+				price = int.Parse(response);
+			}
+			else
+			{
+				price = 0;
+			}
+
+			if (update)
+				cache[templateId].Update(price);
+			else
+				cache[templateId] = new CachePrice(price);
+
+			return price;
+		}
+	}
+
+	internal class CachePrice
+	{
+		public int price { get; private set; }
+		public DateTime lastUpdate { get; private set; }
+
+		public CachePrice(int price)
+		{
+			this.price = price;
+			lastUpdate = DateTime.Now;
+		}
+
+		public void Update(int price)
+		{
+			this.price = price;
+			lastUpdate = DateTime.Now;
+		}
+	}
+}
