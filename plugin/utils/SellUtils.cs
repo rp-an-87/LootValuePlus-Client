6067fc0e78b2f93e69311909350c252ad37dee57
RP <rodrigo-peralta@hotmail.com.ar>
2024-08-05 19:46:49 +0200

    log changes

--- plugin/utils/SellUtils.cs
+++ plugin/utils/SellUtils.cs
@@ -228,8 +228,7 @@ namespace LootValuePlus
 			var offerRequeriment = new FleaRequirement()
 			{
 				count = price, //undercut by 1 ruble
-				_tpl = "5449016a4bdc2d6f028b456f", //id of ruble
-				onlyFunctional = true
+				_tpl = "5449016a4bdc2d6f028b456f" //id of ruble
 			};
 
 			Globals.logger.LogInfo($"Invoking Session.RagFair.AddOffer(false, '{itemIds.EnumerableToString(", ")}', ({offerRequeriment.ToPrettyJson()}), null)");
