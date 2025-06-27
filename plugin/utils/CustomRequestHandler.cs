4d4174ac48e0f8f8cf8c1f8530efdb33016e625f
RP <rodrigo-peralta@hotmail.com.ar>
2025-06-27 03:55:56 +0200

    remove unneeded code

--- plugin/utils/CustomRequestHandler.cs
+++ plugin/utils/CustomRequestHandler.cs
@@ -35,56 +35,27 @@ namespace LootValuePlus
             HttpClient = new Client(Host, SessionId);
         }
 
-        private static void ValidateData(string path, byte[] data) { }
 
-        private static void ValidateJson(string path, string json) { }
-
-        public static async Task<byte[]> GetDataAsync(string path)
+        private static void ValidateJson(string path, string json)
         {
-            byte[] array = await HttpClient.GetAsync(path);
-            ValidateData(path, array);
-            return array;
+
         }
 
-        public static byte[] GetData(string path)
-        {
-            return Task.Run(() => GetData(path)).Result;
-        }
-
-        public static async Task<string> GetJsonAsync(string path)
-        {
-            byte[] bytes = await HttpClient.GetAsync(path);
-            string @string = Encoding.UTF8.GetString(bytes);
-            ValidateJson(path, @string);
-            return @string;
-        }
 
         public static async Task<string> PostJsonAsync(string path, string json)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(json);
             byte[] bytes2 = await HttpClient.PostAsync(path, bytes);
+
+            if (null == bytes2)
+            {
+                return null;
+            }
+
             string @string = Encoding.UTF8.GetString(bytes2);
             ValidateJson(path, @string);
             return @string;
         }
 
-        public static string PostJson(string path, string json)
-        {
-            return PostJson(path, json);
-        }
-
-        public static async Task<string> PutJsonAsync(string path, string json)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(json);
-            byte[] bytes2 = await HttpClient.PutAsync(path, bytes);
-            string @string = Encoding.UTF8.GetString(bytes2);
-            ValidateJson(path, @string);
-            return @string;
-        }
-
-        public static string PutJson(string path, string json)
-        {
-            return Task.Run(() => PutJsonAsync(path, json)).Result;
-        }
     }
 }
